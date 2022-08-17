ee1000e89d19e981f736143804f7a45132c0ae41
EhWanu <cameronjshadgett@gmail.com>
2022-08-17 12:41:42 -0400

    added javascript for hamburger transition

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("test");
+// DECLARATIONS
+
+const hamburger =
+	document.querySelector("#hamburger");
+
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", () => {
+	menu.classList.toggle("hidden");
+	hamburger.classList.toggle("bg-white");
+});
