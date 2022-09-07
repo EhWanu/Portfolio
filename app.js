2558430ed9c188cffd02a098e222c59c66dc1655
EhWanu <cameronjshadgett@gmail.com>
2022-09-07 11:36:45 -0400

    basic JavaScript for closing hamburger when clicked on link

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const hamburger =
 	document.querySelector("#hamburger");
 
 const menu = document.querySelector("#menu");
+const hLinks = document.querySelectorAll("#menu");
 
 hamburger.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
 	hamburger.classList.toggle("bg-white");
+});
+
+hLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		menu.classList.toggle("hidden");
+		hamburger.classList.toggle("bg-white");
+	});
 });
