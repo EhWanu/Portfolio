60fa1c73811e3642a3c6a9d0e90cf285102fc2da
EhWanu <cameronjshadgett@gmail.com>
2022-09-08 11:45:18 -0400

    code refactor

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// DECLARATIONS
-
 const hamburger =
 	document.querySelector("#hamburger");
-
 const menu = document.querySelector("#menu");
-const hLinks = document.querySelectorAll("#menu");
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+const hLinks =
+	document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
@@ -16,4 +16,8 @@ hLinks.forEach((link) => {
 		menu.classList.toggle("hidden");
 		hamburger.classList.toggle("bg-white");
 	});
+});
+
+moon.addEventListener("click", () => {
+	body.classList.toggle("dark");
 });
